c8a2a102f7f2bd89a82962745dbd401124904cde
JiHoon0129 <jihoon0129@users.noreply.github.com>
2026-09-23 20:16:11 +0900

    Add lunar javascript type declaration

---
+++ app/api/saju/lunar-javascript.d.ts
@@ -0,0 +1,12 @@
+declare module "lunar-javascript" {
+  export const Solar: any;
+  export const Lunar: any;
+  export const EightChar: any;
+  export const LunarYear: any;
+  export const SolarYear: any;
+  export const HolidayUtil: any;
+  export const LunarMonth: any;
+  export const LunarDay: any;
+  export const SolarMonth: any;
+  export const SolarDay: any;
+}
